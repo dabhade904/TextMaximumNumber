cd34b6027568ffad57db3670cfccf176661933f2
dabhade904 <dabhade904@gmail.com>
2020-01-02 16:46:09 +0530

    Added : test case to check flaot largest number in all three position

--- src/main/java/com/bridgelabz/textmaximum/TestMaximum.java
+++ src/main/java/com/bridgelabz/textmaximum/TestMaximum.java
@@ -2,8 +2,19 @@ package com.bridgelabz.textmaximum;
 
 public class TestMaximum {
 
-    public Integer getThreeInpute(Integer firstNumber, Integer secondNumber, Integer thirdNumber) {
+    public Integer getThreeInput(Integer firstNumber, Integer secondNumber, Integer thirdNumber) {
         int max=firstNumber;
+        if(secondNumber.compareTo(max)>0) {
+            max = secondNumber;
+        }
+        if (thirdNumber.compareTo(max)>0) {
+            max = thirdNumber;
+        }
+        return max;
+
+    }
+    public Float getThreeFloatInput(Float firstNumber, Float secondNumber, Float thirdNumber) {
+        Float max=firstNumber;
         if(secondNumber.compareTo(max)>0) {
             max = secondNumber;
         }
--- src/test/java/com/bridgelabz/test/TestMaximumTest.java
+++ src/test/java/com/bridgelabz/test/TestMaximumTest.java
@@ -8,24 +8,44 @@ import org.junit.Test;
 public class TestMaximumTest {
     private TestMaximum testMaximum;
     @Before
+
     public void setUp(){
         testMaximum=new TestMaximum();
     }
+
     @Test
     public void whenGivenMaxNumber_IfFirstPosition_ShouldReturnSameNumber(){
-        int inpute=testMaximum.getThreeInpute(30,20,10);
+        int inpute=testMaximum.getThreeInput(30,20,10);
         Assert.assertEquals(30,inpute);
     }
 
     @Test
-    public void whenGivenMaxNumber_IfSecondPoasition_ShouldReturnSameNumber(){
-        int inpute=testMaximum.getThreeInpute(20,30,10);
+    public void whenGivenMaxNumber_IfSecondPosition_ShouldReturnSameNumber(){
+        int inpute=testMaximum.getThreeInput(20,30,10);
         Assert.assertEquals(30,inpute);
     }
 
     @Test
-    public void whenGivenMaxNummber_IfThirdPosition_ShouldReturnSameNumber(){
-        int inpute=testMaximum.getThreeInpute(10,20,30);
+    public void whenGivenMaxNumber_IfThirdPosition_ShouldReturnSameNumber(){
+        int inpute=testMaximum.getThreeInput(10,20,30);
         Assert.assertEquals(30,inpute);
+    }
+
+    @Test
+    public void whenGivenFloatMaxNumber_IfFirstPosition_ShouldReturnSameNumber(){
+        float input=testMaximum.getThreeFloatInput(30.11f,20.33f,10.33f);
+        Assert.assertEquals(30.11f,input,0.0);
+    }
+
+    @Test
+    public void whenGivenFloatMaxNumber_IfSecondPosition_ShouldReturnSameNumber(){
+        float input=testMaximum.getThreeFloatInput(20.55f,30.11f,10.33f);
+        Assert.assertEquals(30.11f,input,0.0);
+    }
+
+    @Test
+    public void whenGivenFloatMaxNumber_IfThirdPosition_ShouldReturnSameNumber(){
+        float input=testMaximum.getThreeFloatInput(10.11f,20.33f,30.11f);
+        Assert.assertEquals(30.11f,input,0.0);
     }
 }
